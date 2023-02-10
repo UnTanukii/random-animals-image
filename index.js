b8e1dffea06042361df5d005f64bce43347d0a4b
Tanukii <79363399+UnTanukii@users.noreply.github.com>
2023-02-10 22:36:31 +0100

    v1.0.7

--- index.js
+++ index.js
@@ -1,55 +1,17 @@
 const fetch = require('node-fetch');
 
-module.exports.cat = async function(){
-    const response = await fetch('https://some-random-api.ml/img/cat')
+async function GetAnimal(name){
+    const response = await fetch('https://some-random-api.ml/img/' + name)
     var json = await response.json()
     return json.link
 }
 
-module.exports.dog = async function(){
-    const response = await fetch('https://some-random-api.ml/img/dog')
-    var json = await response.json()
-    return json.link
-}
-
-module.exports.panda = async function(){
-    const response = await fetch('https://some-random-api.ml/img/panda')
-    var json = await response.json()
-    return json.link
-}
-
-module.exports.fox = async function(){
-    const response = await fetch('https://some-random-api.ml/img/fox')
-    var json = await response.json()
-    return json.link
-}
-
-module.exports.koala = async function(){
-    const response = await fetch('https://some-random-api.ml/img/koala')
-    var json = await response.json()
-    return json.link
-}
-
-module.exports.redpanda = async function(){
-    const response = await fetch('https://some-random-api.ml/img/red_panda')
-    var json = await response.json()
-    return json.link
-}
-
-module.exports.bird = async function(){
-    const response = await fetch('https://some-random-api.ml/img/bird')
-    var json = await response.json()
-    return json.link
-}
-
-module.exports.raccoon = async function(){
-    const response = await fetch('https://some-random-api.ml/animal/raccoon')
-    var json = await response.json()
-    return json.image
-}
-
-module.exports.kangaroo = async function(){
-    const response = await fetch('https://some-random-api.ml/animal/kangaroo')
-    var json = await response.json()
-    return json.image
-}
+module.exports.cat = await GetAnimal('cat')
+module.exports.dog = await GetAnimal('dog')
+module.exports.panda = await GetAnimal('panda')
+module.exports.fox = await  GetAnimal('fox')
+module.exports.koala = await GetAnimal('koala')
+module.exports.redpanda = await GetAnimal('red_panda')
+module.exports.bird = await GetAnimal('bird')
+module.exports.raccoon = await GetAnimal('raccoon')
+module.exports.kangaroo = await GetAnimal('kangaroo')
